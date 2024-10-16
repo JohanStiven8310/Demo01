9829a2a1fa88bc389fa411cb48c5e347c43d8499
JohanStiven8310 <jhanstivn93@gmail.com>
2024-10-16 17:54:47 -0500

    Mylibrary - MessageWriter

---
+++ MyLibrary/MessageWriter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+ 
+    
+        public class MessageWriter
+        {
+            public void Write(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
---
+++ MyconsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyconsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
