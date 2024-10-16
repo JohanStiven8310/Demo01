5c78b48ad0dd8e084e2633d785a469f0e3e941a1
JohanStiven8310 <jhanstivn93@gmail.com>
2024-10-16 17:56:21 -0500

    MyConsoleApp - Program

--- MyconsoleApp/Program.cs
+++ MyconsoleApp/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace MyconsoleApp
+﻿
+
+using MyLibrary;
+namespace MyConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            MessageWriter MyWriter = new MessageWriter();
+            MyWriter.Write("Hello Writer!");
         }
     }
 }
